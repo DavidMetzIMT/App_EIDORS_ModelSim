4825472affdae0b61b2e1292ef90f307ec222c5f
David Metz <david.metz67@gmail.com>
2022-02-02 18:02:17 +0100

    Add cylinder as dummy object

--- Sub_Programms/Sim_GenerateModelwithCell.m
+++ Sub_Programms/Sim_GenerateModelwithCell.m
@@ -9,37 +9,32 @@ close all
 
 fmdl = EIDORS.fmdl; %% buffer fmdl
 
-switch EIDORS.flag.Object
-    case 'Cell'
-        if EIDORS.flag.AddcellinFEMmodel % rebuild fwd model with cell
-            text='';
+if EIDORS.flag.AddcellinFEMmodel % rebuild fwd model with cell
+    text='';
+    switch EIDORS.flag.Object
+        case 'Cell'
             for i=1:size(EIDORS.sim.cell,1)
                 c=EIDORS.sim.cell(i);
                 text= [text '\n\r ' 'solid cell_' num2str(i) ' = sphere(' num2str(c.PosX) ',' num2str(c.PosY) ',' num2str(c.PosZ) ';' num2str(c.Radius) ');\n\r ' 'tlo cell_' num2str(i) ';'];
             end
-
-            EIDORS.sim.netgenAdditionalText=text;
-            create_fmdl(); % new fmdl in EIDORS.fmdl
-            h= gcf;
-            set(h,'Name','Foward Model with cell' )
-        end
-    
-    case 'Cylinder'
-        if EIDORS.flag.AddcellinFEMmodel
-            text='';
+            fig_name='Foward Model with cell';
+            
+        case 'Cylinder'
             for i=1:size(EIDORS.sim.cell,1)
                 c=EIDORS.sim.cell(i);
-                text=[['wall    = cylinder (0,0,0; 0,0,1;' num2str(c.Radius) '); \n'], ...
-                        'top    = plane(0,0,' num2str(EIDORS.chamber.body.depth/2) ';0,0,1);\n' ...
-                        'bottom = plane(0,0,-' num2str(c.PosZ) ';0,0,-1);\n' ...
-                        'mainobj= top and bottom and wall -maxh=' num2str( EIDORS.chamber.body.FEM_refinement) ';\n\r' 'tlo mainobj' ';'];
+                text=['solid wall_cyc = cylinder (0,0,0; 0,0,1;' num2str(c.Radius) ');\n' ...
+                    'solid top_cyc = plane(0,0,' num2str(EIDORS.chamber.body.depth/2-0.1) ';0,0,1);\n' ...
+                    'solid bottom_cyc = plane(0,0,-' num2str(c.PosZ) ';0,0,-1);\n' ...
+                    'solid mainobj_cyc = top_cyc and bottom_cyc and wall_cyc -maxh=' num2str( EIDORS.chamber.body.FEM_refinement/2) ';\n' 'tlo mainobj_cyc' ';\n']
             end
-
-            EIDORS.sim.netgenAdditionalText=text;
-            create_fmdl(); % new fmdl in EIDORS.fmdl
-            h= gcf;
-            set(h,'Name','Foward Model with cylinder' )
-        end
+            fig_name= 'Foward Model with cylinder';
+            
+    end
+    
+    EIDORS.sim.netgenAdditionalText=text;
+    create_fmdl(); % new fmdl in EIDORS.fmdl
+    h= gcf;
+    set(h,'Name',fig_name )
 end
 
 EIDORS.sim.fmdl = EIDORS.fmdl;
@@ -59,19 +54,28 @@ EIDORS.sim.img_h = mk_image(EIDORS.sim.fmdl, EIDORS.sim.bufferConduct);
 
 
 %% Set inhomogenious conductivity
+
 for i=1:size(EIDORS.sim.cell,1)
     c=EIDORS.sim.cell(i);
-    select_cell = @(x,y,z) (x-c.PosX).^2 + (y-c.PosY).^2+(z-c.PosZ).^2 <= c.Radius^2;
+    switch EIDORS.flag.Object
+        case 'Cell'
+            select_cell = @(x,y,z) (x-c.PosX).^2 + (y-c.PosY).^2+(z-c.PosZ).^2 <= c.Radius^2;
+            
+        case 'Cylinder'
+            select_cell = @(x,y,z) (x-c.PosX).^2 + (y-c.PosY).^2 <= (ones(size(y))*c.Radius).^2 & z>=c.PosZ;
+    end
     cell_frac(:,i) = elem_select( EIDORS.sim.img_h.fwd_model, select_cell);
-    select_rst = @(x,y,z) (x-c.PosX).^2 + (y-c.PosY).^2+(z-c.PosZ).^2 > c.Radius^2 ;
-    layer_frac(:,i) = elem_select( EIDORS.sim.img_h.fwd_model, select_rst);%% do we really need this one???
-    
+% select_rst = @(x,y,z) (x-c.PosX).^2 + (y-c.PosY).^2+(z-c.PosZ).^2 > c.Radius^2 ;
+% layer_frac(:,i) = elem_select( EIDORS.sim.img_h.fwd_model, select_rst);%% do we really need this one???
 end
+
+
+
 if size(EIDORS.sim.cell,1)==0
     EIDORS.sim.img_ih=EIDORS.sim.img_h;
 else
     c_is=sum(cell_frac>0,2);
-    l_is=sum(layer_frac>0,2);
+%     l_is=sum(layer_frac>0,2);
     for i=1:size(cell_frac,1)
         if c_is(i)==0
             c_frac(i,1) =0;
@@ -80,14 +84,14 @@ else
         elseif c_is(i)>1
             c_frac(i,1) = 1;
         end
-        if sum(layer_frac(i,:),2)==size(layer_frac,2)
-            l_frac(i,1) =1;
-        else
-            l_frac(i,1) = 0;
-        end
+%         if sum(layer_frac(i,:),2)==size(layer_frac,2)
+%             l_frac(i,1) =1;
+%         else
+%             l_frac(i,1) = 0;
+%         end
     end
     
-    EIDORS.sim.img_ih = mk_image(EIDORS.sim.img_h, EIDORS.sim.bufferConduct + c_frac*(EIDORS.sim.cellConduct) + l_frac*(EIDORS.sim.layerConduct));
+%     EIDORS.sim.img_ih = mk_image(EIDORS.sim.img_h, EIDORS.sim.bufferConduct + c_frac*(EIDORS.sim.cellConduct) + l_frac*(EIDORS.sim.layerConduct));
     EIDORS.sim.img_ih = mk_image(EIDORS.sim.img_h, EIDORS.sim.bufferConduct + c_frac*(EIDORS.sim.cellConduct));
 end
 
@@ -113,4 +117,9 @@ if ~EIDORS.flag.displaymesh
 end
 show_cell(h,EIDORS.sim.iimg.calc_slices.levels,1)
 disp('Generate model with cell: Done!')
+
+
+end 
+
+
 
