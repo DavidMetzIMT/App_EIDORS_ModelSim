891679ccc9b44f922fdff00ca58ea04f58e9306c
David Metz <david.metz67@gmail.com>
2022-02-25 13:03:35 +0100

    get rip of filesep at the end  of  the folder

--- Sub_Programms/utils/path_join.m
+++ Sub_Programms/utils/path_join.m
@@ -1,4 +1,7 @@
 function path = path_join(folder, filename) 
-    % Create a default Path 
+    % Create a default Path
+    if strcmp(folder(end), filesep)
+        folder= folder(1:end-1);
+    end
     path= [folder filesep filename];
 end
