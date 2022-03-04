6381be59497e63e54d5d6bd639574a4d905b0fb8
David Metz <david.metz67@gmail.com>
2022-03-04 12:24:32 +0100

    fix num_array2str for 2d array

--- src/utils/num_array2str.m
+++ src/utils/num_array2str.m
@@ -1,4 +1,13 @@
 function s= num_array2str(array)
     %NUM_ARRAY2STR Return the array as a string as "[ array ]"
-    s = [ '[' num2str(array) ']'];
+    
+    s= '';
+    n_row=size(array,1);
+    for row = 1:n_row
+        s= [s, num2str(array(row,:))];
+        if row~=n_row
+            s=[s '; '];
+        end
+    end
+    s = [ '[' s ']'];
 end
